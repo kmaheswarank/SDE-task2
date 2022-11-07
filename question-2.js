8279bddab100d54c9344dd8414c26d43310f9df5
kmaheswarank <115221899+kmaheswarank@users.noreply.github.com>
2022-11-07 11:40:12 +0530

    Create question-2.js

---
+++ question-2.js
@@ -0,0 +1,10 @@
+# SDE-task2
+let cargoHold = ['oxygen tanks', 'space suits', 'parrot', 'instruction manual', 'meal packs', 'slinky', 'security blanket'];
+
+cargoHold[5] = 'space tether';
+console.log(cargoHold);
+
+console.log(cargoHold.shift());
+console.log(cargoHold);
+
+console.log(`The array ${cargoHold} has a length of ${cargoHold.length}.`);
