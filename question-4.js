5d2abd036ab03a14aa00e1f1f8bcde258bbd73c1
kmaheswarank <115221899+kmaheswarank@users.noreply.github.com>
2022-11-07 11:51:50 +0530

    Create question-4.js

---
+++ question-4.js
@@ -0,0 +1,10 @@
+# SDE-task2
+
+holdCabinet1 ['duct tape', 'gum', 3.14, false, 6.022e23]
+holdCabinet2 ['orange drink', 'nerf toys', 'camera', 42, 'parsnip']
+console.log(holdCabinet1.concat(holdCabinet2));
+console.log(holdCabinet1);
+holdCabinet1.reverse();
+holdCabinet2.sort();
+console.log(holdCabinet1);
+console.log(holdCabinet2);
